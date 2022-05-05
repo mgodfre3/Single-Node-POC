856880e9725521fa0c5bee06bd44d181af0c14e8
Michael Godfrey <migodfre@microsoft.com>
2022-05-05 09:55:11 -0400

    corrected line 42 Test to Temp & Added better S2D functionality.

--- Single-NodeHC-NoDomain/SingleNodeBuild-Step2.ps1
+++ Single-NodeHC-NoDomain/SingleNodeBuild-Step2.ps1
@@ -1,11 +1,12 @@
+##Parmaeters
 $LocalCreds=Get-Credential -UserName "SAHCI\Administrator" -Message "Provide Local Credentials"
-$HCInode=""
-$HCINNodeName="SAHCI"
-$Clustername="SAHCICL"
-$DNSIp=""
-$clusterIP="1"
+$HCInode="" #Provide HCI Node Name or if no DNS entry is populated, the IP address will work. 
+$HCINNodeName="" # Provide the desired name of the Node
+$Clustername="" #Provide the Desired Cluster Name
+$SPFName="" #Provide Friendly name for Storage Pool
+$DNSIp=""   #Provide the Desired DNS Server(s) IP Addresses
+$clusterIP=""   #Provide the Desired Cluster IP Address, please update your DNS with A Records
 
-$AZSubscriptionIS=""
 
 
 
@@ -16,11 +17,10 @@ Invoke-Command -ComputerName $HCINode -Credential $localcreds -ScriptBlock {
     $vnetadapter=Get-Netadapter |  Where-Object {$_.name -Like "vEthernet*" -and $_.status -eq "Up"}
     Set-DnsClientServerAddress -InterfaceAlias $vnetadapter.InterfaceAlias -ServerAddresses $using:dnsip 
     New-Cluster -Name SAHCICL -StaticAddress $using:clusterIP -AdministrativeAccessPoint dns -NoStorage
-    Enable-ClusterS2D -PoolFriendlyName "SAHCICL Storage Pool"
-    $sp=Get-StoragePool | Where-Object friendlyname -ne "Primordial" 
-    New-Volume -FriendlyName "Volume 1" -StoragePoolFriendlyName $sp.FriendlyName -FileSystem CSVFS_ReFS -ResiliencySettingName Simple -Size 100GB
-    
-    
+    Enable-ClusterS2D -PoolFriendlyName $SPFName -Confirm:$false
+    Set-StoragePool -FriendlyName $SPFName -FaultDomainAwarenessDefault 'PhysicalDisk'
+    New-Volume -StoragePoolFriendlyName $SPFName -FriendlyName "Volume01" -FileSystem CSVFS_ReFS `
+     -ResiliencySettingName Simple -UseMaximumSize
     
     }
 
@@ -39,7 +39,7 @@ write-host -ForegroundColor Green -Object "Register the Cluster to Azure Subscri
         Remove-Item 'C:\Program Files\WindowsPowerShell\Modules\Az.StackHCI' -Recurse -force
         Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
         $hcicustommoduleuri="https://github.com/mgodfre3/Single-Node-POC/blob/main/Single-NodeHC-NoDomain/CustomModules.zip?raw=true"
-        New-Item -Path C:\ -Name Test -ItemType Directory
+        New-Item -Path C:\ -Name Temp -ItemType Directory
         Invoke-WebRequest -Uri $hcicustommoduleuri -OutFile C:\temp\Az.StackHCI-Custom.zip
         Expand-Archive C:\temp\AZ.StackHCI-Custom.zip -DestinationPath 'C:\Temp\Custom' -Force
         Get-Item C:\temp\Custom\CustomModules\Az.StackHCI |Copy-Item -Destination 'C:\Program Files\WindowsPowerShell\Modules' -Recurse -force
